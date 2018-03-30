41ad58bdb655ebb98a504669d723a74d14e557a4
Theara Seng <seng.theara.kh@gmail.com>
2018-03-30 13:46:18 +0700

    implement Logging Listener

--- src/main/java/com/theara/rabbitmq/rabbitmqconsumer/config/RabbitMqConfig.java
+++ src/main/java/com/theara/rabbitmq/rabbitmqconsumer/config/RabbitMqConfig.java
@@ -18,30 +18,41 @@ public class RabbitMqConfig {
 
     public static final String ROUTING_KEY = "server1.app1.module1.info";
     public static final String ERROR_ROUTING_KEY = "server1.app1.module1.error";
+    public static final String LOGGING_ROUTING_KEY = "server1.app1.module1.*";
 
     @Bean("infoQueue")
-    Queue infoQueue() {
+    public Queue infoQueue() {
         return new Queue(ROUTING_KEY, true);
     }
 
     @Bean("errorQueue")
-    Queue errorQueue() {
+    public Queue errorQueue() {
         return new Queue(ERROR_ROUTING_KEY, true);
     }
 
+    @Bean("loggingQueue")
+    public Queue logQueue() {
+        return new Queue(LOGGING_ROUTING_KEY, true);
+    }
+
     @Bean
-    TopicExchange topicExchange() {
+    public TopicExchange topicExchange() {
         return new TopicExchange("my_queue_exchange");
     }
 
     @Bean
-    Binding infoQueueBinding(@Qualifier("infoQueue") Queue queue, TopicExchange topicExchange) {
+    public Binding infoQueueBinding(@Qualifier("infoQueue") Queue queue, TopicExchange topicExchange) {
         return BindingBuilder.bind(queue).to(topicExchange).with(ROUTING_KEY);
     }
 
     @Bean
-    Binding errorQueueBinding(@Qualifier("errorQueue") Queue queue, TopicExchange topicExchange) {
+    public Binding errorQueueBinding(@Qualifier("errorQueue") Queue queue, TopicExchange topicExchange) {
         return BindingBuilder.bind(queue).to(topicExchange).with(ERROR_ROUTING_KEY);
+    }
+
+    @Bean
+    public Binding loggingQueueBinding(@Qualifier("loggingQueue") Queue queue, TopicExchange topicExchange) {
+        return BindingBuilder.bind(queue).to(topicExchange).with(LOGGING_ROUTING_KEY);
     }
 
     @Bean
@@ -61,6 +72,17 @@ public class RabbitMqConfig {
         SimpleMessageListenerContainer container = new SimpleMessageListenerContainer();
         container.setConnectionFactory(connectionFactory);
         container.setQueueNames(ERROR_ROUTING_KEY);
+        container.setMessageListener(new MessageListenerAdapter(listener, "onMessage"));
+
+        return container;
+    }
+
+    @Bean
+    public SimpleMessageListenerContainer loggingMessageListenerContainer(ConnectionFactory connectionFactory, @Qualifier("loggingMessageListener") MessageListener listener) {
+
+        SimpleMessageListenerContainer container = new SimpleMessageListenerContainer();
+        container.setConnectionFactory(connectionFactory);
+        container.setQueueNames(LOGGING_ROUTING_KEY);
         container.setMessageListener(new MessageListenerAdapter(listener, "onMessage"));
 
         return container;
